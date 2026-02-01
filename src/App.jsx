ad5ba1f4c9dcf549245ad18ccc5bfb1eefe95f05
Julianvargas97 <jvargas97@hotmail.com>
2026-02-01 01:10:18 -0500

    fix: correct Firebase storage bucket URL and remove duplicate import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { initializeApp } from "firebase/app";
 import { getFunctions } from "firebase/functions";
 import FormularioCiudadano from "./components/FormularioCiudadano";
-import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAj95PSPPx8qz9ReYrFyLiKu9E2knszShs",
   authDomain: "formulario-vinculacion-4f870.firebaseapp.com",
   projectId: "formulario-vinculacion-4f870",
-  storageBucket: "formulario-vinculacion-4f870.firebasestorage.app",
+  storageBucket: "formulario-vinculacion-4f870.appspot.com",
   messagingSenderId: "772408250491",
   appId: "1:772408250491:web:0ab86aa13997952550d2de"
 };
@@ -14,10 +14,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Functions
 const functions = getFunctions(app);
 
 export { app, functions };
-
 
 function App() {
   return <FormularioCiudadano />;
